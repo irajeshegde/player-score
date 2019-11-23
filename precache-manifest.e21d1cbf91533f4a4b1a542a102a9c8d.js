86f2762b9cc7545f4e479bb3389148095b274114
irajeshegde <rajeshhegde180@gmail.com>
2019-11-23 19:19:43 +0530

    Updates

---
+++ precache-manifest.e21d1cbf91533f4a4b1a542a102a9c8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "101d9c8cc83a1467f820626576efa252",
+    "url": "/player-score/index.html"
+  },
+  {
+    "revision": "f22b05b9cc74f99bae07",
+    "url": "/player-score/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "106849ca4d3e0ec5cf33",
+    "url": "/player-score/static/js/2.556b73d4.chunk.js"
+  },
+  {
+    "revision": "f22b05b9cc74f99bae07",
+    "url": "/player-score/static/js/main.061b07c9.chunk.js"
+  },
+  {
+    "revision": "e0b2bb3ba6e9b4f84084",
+    "url": "/player-score/static/js/runtime-main.6b995c10.js"
+  }
+]);
